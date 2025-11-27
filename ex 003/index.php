82d45a7c0802b1616d3eb5f8423bdec412341789
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-11-27 00:12:54 -0300

    atualizado

---
+++ ex 003/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+    $num = 0x1A;
+    echo "O valor da variavel é $num"; 
+    var_dump($num)
+    ?>
+</body>
+</html>
