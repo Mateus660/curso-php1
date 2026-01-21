779c2345c07097f4e370dfd212aa8d1fa4229892
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2026-01-20 23:47:33 -0300

    atualizado

---
+++ ex 005/superglobais.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <pre>
+            <?php 
+             echo "<h1> superglobal get </h1>";
+             var_dump(()$_GET)
+            ?>
+        </pre>
+    </main>
+</body>
+</html>
